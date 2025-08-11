185a9aa705beb21fa18a7375c7d892007f1a3b04
Stercy Mutuku <mutukustercy93@gmail.com>
2025-08-11 13:01:30 +0300

    for and while in python

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i = i - 1
+    print("Happy New Year!")
+   
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    items = []
+    for item in int_list: 
+        items.append(item * item)
+    return items
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
